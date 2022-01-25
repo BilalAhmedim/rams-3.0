e2c3112b3f7fed41a99e1bf2efde14a09c58dbfc
Bilal Ahmed <bilalahmedim@gmail.com>
2022-01-25 17:44:54 +0530

    sourcemap added

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
-module.exports = {
+module.exports = (ctx) => ({
+  map: ctx.options.map,
   plugins: [
+    require("sourcemap"),
     require("postcss-import"),
     require("postcss-nested"),
     require("postcss-mixins"),
     require("postcss-simple-vars"),
     require("postcss-preset-env"),
     require("autoprefixer"),
-    // require("cssnano"),
+    require("cssnano"),
   ],
-};
+});
