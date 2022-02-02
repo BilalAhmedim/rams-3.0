ded6a7dbf731777bf676f0537a295de6804a3697
Bilal Ahmed <bilalahmedim@gmail.com>
2022-02-02 23:33:59 +0530

    double click conflict

--- App/js/custom.js
+++ App/js/custom.js
@@ -1,13 +1,17 @@
 const parentmenu = document.querySelectorAll(".contain-submenu");
 
 const toggleMenu = (event) => {
+  const expand = document.querySelector(".expand");
   const li = event.target.parentElement;
   const toggleclass = "expand";
-  const siblingul = event.target.nextElementSibling;
   event.preventDefault();
+  console.log(expand); // laga
+  expand == undefined ? "" : expand.classList.remove(toggleclass);
+  console.log(expand); // hata
   li.classList[1] === undefined
     ? li.classList.add(toggleclass)
     : li.classList.remove(toggleclass);
+  console.log(expand); // aya
 };
 
 parentmenu.forEach(
