0d0a7139731df37e0005057b8efa87d16382cca4
Bilal Ahmed <bilalahmedim@gmail.com>
2022-02-03 12:21:44 +0530

    link is working prevent default issue fixed

--- App/js/custom.js
+++ App/js/custom.js
@@ -2,16 +2,15 @@ const parentmenu = document.querySelectorAll(".contain-submenu");
 
 const toggleMenu = (event) => {
   const expand = document.querySelector(".expand");
-  const li = event.target.parentElement;
+  const li = event.target.closest(".contain-submenu"); // or .parentElement
   const toggleclass = "expand";
-  event.preventDefault();
-  console.log(expand); // laga
+  event.target.getAttribute("role") == undefined || null
+    ? event.preventDefault()
+    : "";
   expand == undefined ? "" : expand.classList.remove(toggleclass);
-  console.log(expand); // hata
   li.classList[1] === undefined
     ? li.classList.add(toggleclass)
     : li.classList.remove(toggleclass);
-  console.log(expand); // aya
 };
 
 parentmenu.forEach(
